3b7e83afa9f6fb442c0bbe3da2b969ab375f9439
amin <zainabamin281@gmail.com>
2025-08-24 21:33:51 +0530

    basic

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-hi
+from tkinter import * #creating GUI 
+import tkinter as tk
+from tkinter.filedialog import asksaveasfilename # filedialog for creating files
+
+#create main window
+stimulator_window = Tk()
+stimulator_window.geometry("600x600")
+stimulator_window.title("editorz")
+
+
+
+
+stimulator_window.mainloop()
