ccf8af6988ac7d8eea14945736a923c464198e0c
amin <zainabamin281@gmail.com>
2025-08-25 02:01:38 +0530

    open-file-buttoned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import * #creating GUI 
 import tkinter as tk
-from tkinter.filedialog import asksaveasfilename # filedialog for creating files
+from tkinter import filedialog # filedialog for creating files
 
 #create main window
 main_window = Tk()
@@ -15,6 +15,15 @@ text_area.pack(expand=1, fill="both")
 
 scrollbar.config(command=text_area.yview)
 
+def open_file():
+    file_path = filedialog.askopenfilename(defaultextension=".txt", filetypes=[("text files", "*.txt"), ("all files", "*.*")])
+    if file_path:
+        with open(file_path, "r") as file:
+            text_area.delete("1.0", END)
+            text_area.insert("1.0", file.read())
 
+
+open_button = tk.Button(main_window, text="Open File", command=open_file)
+open_button.pack()
 
 main_window.mainloop()
